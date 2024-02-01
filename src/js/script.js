447a999f8399140446459628704ea3fb557f0dd6
go_ohtawa <go.ohtawa@gmail.com>
2024-02-01 10:44:50 +0900

    画像アニメーション追加中

--- src/js/script.js
+++ src/js/script.js
@@ -70,10 +70,10 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
         fadeEffect: {
             crossFade: true,
         },
-        // autoplay: {
-        //     delay: 4000,
-        //     disableOnInteraction: false,
-        // },
+        autoplay: {
+            delay: 4000,
+            disableOnInteraction: false,
+        },
     });
 
 
@@ -128,10 +128,10 @@ topBtn.click(function () {
       speed: 2000,
       slidesPerView: 1.3,
       spaceBetween: 20,
-      // autoplay: {
-      //     delay: 2000,
-      //     disableOnInteraction: false,
-      // },
+      autoplay: {
+          delay: 2000,
+          disableOnInteraction: false,
+      },
       breakpoints: {
           768: {
               slidesPerView: 3.9,
@@ -145,6 +145,32 @@ topBtn.click(function () {
   });
 
 
+
+  //要素の取得とスピードの設定
+var box = $('.colorbox'),
+speed = 700;  
+
+//.colorboxの付いた全ての要素に対して下記の処理を行う
+box.each(function(){
+$(this).append('<div class="color"></div>')
+var color = $(this).find($('.color')),
+image = $(this).find('img');
+var counter = 0;
+
+image.css('opacity','0');
+color.css('width','0%');
+//inviewを使って背景色が画面に現れたら処理をする
+color.on('inview', function(){
+    if(counter == 0){
+　　　　　$(this).delay(200).animate({'width':'100%'},speed,function(){
+               image.css('opacity','1');
+               $(this).css({'left':'0' , 'right':'auto'});
+               $(this).animate({'width':'0%'},speed);
+            })
+        counter = 1;
+      }
+ });
+});
 
 
 
