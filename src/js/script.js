ea4659bbb0e44ee1d80b918e93a8e5f222fdb617
go_ohtawa <go.ohtawa@gmail.com>
2024-03-16 05:50:20 +0900

    コード zoomFB 修正6回目：TOPページピクパ完了

--- src/js/script.js
+++ src/js/script.js
@@ -29,18 +29,18 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
 /*====================================================
 # mvセクションのスライダー
 // =====================================================*/
-    const mv_swiper = new Swiper(".js-mv-swiper", {
-        loop: true,
-        speed: 2000,
-        effect: "fade",
-        fadeEffect: {
-            crossFade: true,
-        },
-        autoplay: {
-            delay: 4000,
-            disableOnInteraction: false,
-        },
-    });
+    // const mv_swiper = new Swiper(".js-mv-swiper", {
+    //     loop: true,
+    //     speed: 2000,
+    //     effect: "fade",
+    //     fadeEffect: {
+    //         crossFade: true,
+    //     },
+    //     autoplay: {
+    //         delay: 4000,
+    //         disableOnInteraction: false,
+    //     },
+    // });
 
 /*====================================================
 # ボタンをクリックしたらスクロールして上に戻る
@@ -115,10 +115,10 @@ let footHeight = $("footer").innerHeight();
     speed: 2000,
     slidesPerView: "auto",
     spaceBetween: 20,
-    autoplay: {
-        delay: 2000,
-        disableOnInteraction: false,
-    },
+    // autoplay: {
+    //     delay: 2000,
+    //     disableOnInteraction: false,
+    // },
     breakpoints: {
         768: {
             spaceBetween: 42
